fd5341bee67ae257d7aa8eb781bcf343241f46a9
AdebayoAdeoye <adeoyeadaebayo429@gmail.com>
2022-12-19 19:44:39 +0000

    changes have been done

--- login.php
+++ login.php
@@ -12,5 +12,6 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-?>
+
+include<studio.b>}
+Printf ("craeted for merging purpose");
